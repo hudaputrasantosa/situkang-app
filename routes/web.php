356494cb3e871ad2e8e4e38cf50fdc8951e050c5
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-09-08 23:55:20 +0700

    [delete] chnage structure

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Pelanggan\PelangganController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/register', 'register')->name('auth.register');
+    Route::post('/store', 'store')->name('auth.register.store');
+    Route::get('/login', 'login')->name('auth.login');
+    Route::post('/authenticate', 'authenticate')->name('auth.login.process');
 });
 
-require __DIR__.'/auth.php';
+
+Route::controller(PelangganController::class)->group(function () {
+    Route::get('/homepage', 'index')->name('pelanggan.homepage');
+    Route::get('/dashboard', 'register')->name('auth.register');
+});
