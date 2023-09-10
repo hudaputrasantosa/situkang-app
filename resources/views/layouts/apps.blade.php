7ed2f808d74189e1fc1cf6ac7bb5e3e884e5fdd7
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-09-10 23:32:40 +0700

    [update] strukture

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -7,19 +7,17 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Pendaftaran') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-
 </head>
 <body>
     <div id="app">
-      @includeIf('components.navbar', ['status' => 'success']);
-
         <main class="py-4">
             @yield('content')
         </main>
