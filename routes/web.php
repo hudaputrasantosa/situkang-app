6825d0ad72d10df238915009277b9a248026fcb7
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-09-10 23:31:08 +0700

    [update] routes pelanggan, tukang

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Pelanggan\PelangganController;
+use App\Http\Controllers\Tukang\TukangController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,19 +17,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::controller(AuthController::class)->group(function () {
+Route::controller(AuthController::class)->middleware('guest')->group(function () {
     Route::get('/register', 'register')->name('auth.register');
     Route::post('/store', 'store')->name('auth.register.store');
     Route::get('/login', 'login')->name('auth.login');
     Route::post('/authenticate', 'authenticate')->name('auth.login.process');
+    Route::post('/logout', 'logout')->name('auth.logout');
 });
 
 
-Route::controller(PelangganController::class)->group(function () {
-    Route::get('/homepage', 'index')->name('pelanggan.homepage');
-    Route::get('/dashboard', 'register')->name('auth.register');
+
+Route::get('/', [PelangganController::class, 'homepage'])->name('homepage');
+Route::get('/jenis', [PelangganController::class, 'jenis'])->name('jenis');
+Route::get('/tentang', [PelangganController::class, 'tentang'])->name('tentang');
+
+Route::controller(PelangganController::class)->middleware('auth')->group(function () {
+    Route::get('/dashboard', 'dashboard')->name('pelanggan.dashboard');
+    Route::get('/profil', 'profil')->name('pelanggan.profil');
 });
+
+Route::prefix('tukang')->controller(TukangController::class)->group(function () {
+    Route::get('/login', 'index')->name('tukang.login');
+    Route::get('/register', 'register')->name('tukang.register');
+    Route::get('/dashboard', 'dashboard')->name('pelanggan.dashboard')->middleware('auth');
+});
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
