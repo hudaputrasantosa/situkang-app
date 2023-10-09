63c63aaa795ba6bb2c1e681c04aa1c74e43ca973
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-10-09 15:29:15 +0700

    [add] model notification

---
+++ app/Models/Notification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notification';
+    protected $fillable = [
+        'pelanggans_id',
+        'tukangs_id',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            }
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
