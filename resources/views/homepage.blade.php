9fdc35d54bba0f6180c58c69742bab676a7ac392
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-12-12 00:46:18 +0700

    [update] add feature, fix district

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,63 +1,115 @@
 @extends('layouts.app')
 
 @section('content')
-<main>
-<div class="container col-xxl-8 px-4">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-4">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <img src="https://img.freepik.com/free-photo/civil-engineer-construction-worker-architects-wearing-hardhats-safety-vests-are-working-together-construction-site-building-home-cooperation-teamwork-concept_640221-172.jpg?w=740&t=st=1695739276~exp=1695739876~hmac=ee72b6df4c1e90f9731015762bb1397a9420fde5c69f6ebafd8f04d95d1bf36f" class="d-block mx-lg-auto img-fluid rounded" width="600" height="400" loading="lazy">
-      </div>
-      <div class="col-lg-6">
-        <h2 class="display-6 fw-bold lh-1 mb-3">Temukan jenis tukang bangunan sesuai kebutuhan anda.</h2>
-        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components.</p>
-      </div>
-    </div>
-  </div>
+    <main>
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-4">
+                <div class="col-10 col-sm-8 col-lg-6">
+                    <img src="https://img.freepik.com/free-photo/civil-engineer-construction-worker-architects-wearing-hardhats-safety-vests-are-working-together-construction-site-building-home-cooperation-teamwork-concept_640221-172.jpg?w=740&t=st=1695739276~exp=1695739876~hmac=ee72b6df4c1e90f9731015762bb1397a9420fde5c69f6ebafd8f04d95d1bf36f"
+                        class="d-block mx-lg-auto img-fluid rounded" width="600" height="400" loading="lazy">
+                </div>
+                <div class="col-lg-6">
+                    <h2 class="display-6 fw-bold lh-1 mb-3">Temukan jenis tukang bangunan sesuai kebutuhan anda.</h2>
+                    <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s
+                        most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid
+                        system, extensive prebuilt components.</p>
+                </div>
+            </div>
+        </div>
 
-  
+        <div class="container px-4 py-5 mt-4 mx-auto text-center" id="featured-3">
+            <h3 class="pb-2">Cara Kerja SiTukang</h3>
+            <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                <div class="feature col">
+                    <div
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        <svg class="bi" width="1em" height="1em">
+                            <use xlink:href="#collection" />
+                        </svg>
+                    </div>
+                    <h3 class="fs-2">Featured title</h3>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                        and probably just keep going until we run out of words.</p>
 
-  <div class="album py-5 bg-light">
-    <div class="container col-md-12">
-      
-      <div class="py-2 col-5 mb-4 mx-auto text-center">
-  <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Cari Tukang.." aria-describedby="inputGroup-sizing-default">
+                </div>
+                <div class="feature col">
+                    <div
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        <svg class="bi" width="1em" height="1em">
+                            <use xlink:href="#people-circle" />
+                        </svg>
+                    </div>
+                    <h3 class="fs-2">Featured title</h3>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                        and probably just keep going until we run out of words.</p>
 
-  <h4 class="mt-5">Tukang Terdekat</h4>
-</div>
+                </div>
+                <div class="feature col">
+                    <div
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        <svg class="bi" width="1em" height="1em">
+                            <use xlink:href="#toggles2" />
+                        </svg>
+                    </div>
+                    <h3 class="fs-2">Featured title</h3>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                        and probably just keep going until we run out of words.</p>
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-2 gap-2">
-@foreach ($tukangs as $tukang )
-<div class="card m-2" style="width: 16rem;">
-  <a href="{{ route('tukang.portofolio', $tukang->id) }}" class="text-decoration-none text-body">
-    <div class="img-thumbnail aspect-ratio aspect-ratio-1x1 mt-1">
-      <img src="@if($tukang->foto){{ url('storage/tukang/foto-profil/'.$tukang->foto) }} @else https://t3.ftcdn.net/jpg/05/00/54/28/360_F_500542898_LpYSy4RGAi95aDim3TLtSgCNUxNlOlcM.jpg @endif"  class="card-img-top aspect-ratio-object-cover" style="object-fit: cover; object-position: center;">
-  </div>
-<div class="card-body">
-  <h5 class="card-title">{{ $tukang->nama }}</h5>
-  <div class="row d-flex align-items-center">
-    
-    <p class="mb-0" style="font-size: 10pt;">
-      <i class="bi bi-geo-alt-fill">
-      </i> 
-      {{ $tukang->desa }}, {{ $tukang->kecamatan }}</p>
-  </div>
-  <div class="mb-0">
-    <p>{{ $tukang->nama_keahlian}}</p>
-    <p class="fs-5">Rp {{ $tukang->harga }} /Hari</p>
-  </div>  
-</div>
-</a>
-</div>
-@endforeach
+                </div>
+            </div>
+        </div>
 
-        
-      </div>
-    </div>
-  </div>
 
-</main>
+        <div class="album py-3 bg-light">
+            <div class="py-2 col-5 mb-4 mx-auto text-center">
+                {{-- <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Cari Tukang.." aria-describedby="inputGroup-sizing-default"> --}}
+                <h4 class="mt-5">Tukang Terdekat</h4>
+            </div>
+            <div class="container col-md-12">
+
+
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-2 gap-2">
+                    @foreach ($tukangs as $tukang)
+                        <div class="card m-2" style="width: 16rem;">
+                            <a href="{{ route('tukang.portofolio', $tukang->id) }}" class="text-decoration-none text-body">
+                                <div class="img-thumbnail aspect-ratio aspect-ratio-1x1 mt-1">
+                                    <img src="@if ($tukang->foto) {{ url('storage/tukang/foto-profil/' . $tukang->foto) }} @else https://t3.ftcdn.net/jpg/05/00/54/28/360_F_500542898_LpYSy4RGAi95aDim3TLtSgCNUxNlOlcM.jpg @endif"
+                                        class="card-img-top aspect-ratio-object-cover"
+                                        style="object-fit: cover; object-position: center;">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $tukang->nama }}</h5>
+                                    <div class="row d-flex align-items-center">
+
+                                        <p class="mb-0" style="font-size: 10pt;">
+                                            <i class="bi bi-geo-alt-fill">
+                                            </i>
+                                            @php
+                                                $kecamatan = Laravolt\Indonesia\Models\District::where('id', $tukang->kecamatan)->first();
+                                                $desa = Laravolt\Indonesia\Models\Village::where('id', $tukang->desa)->first();
+                                            @endphp
+
+                                            {{ ucwords(strtolower($kecamatan->name)) }},
+                                            {{ ucwords(strtolower($desa->name)) }}
+                                        </p>
+                                    </div>
+                                    <div class="mb-0">
+                                        <p>{{ $tukang->nama_keahlian }}</p>
+                                        <p class="fs-5">Rp {{ $tukang->harga }} /Hari</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+
+
+                </div>
+            </div>
+        </div>
+
+    </main>
 @endsection
 
 @section('js')
-<script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 @endsection
--- resources/views/portofolio.blade.php
+++ resources/views/portofolio.blade.php
@@ -1,192 +1,199 @@
 @extends('layouts.app')
 @section('head-section')
-<link href="{{ asset('assets/css/heroes.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/heroes.css') }}" rel="stylesheet">
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-      }
 
-      .b-example-divider {
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
 
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
+        .b-example-divider {
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+        }
 
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
+        .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+        }
 
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
+        .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+        }
 
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
+        .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+        }
+
+        .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+        }
     </style>
 @endsection
 @section('content')
-<h1 class="visually-hidden">Heroes examples</h1>
+    <h1 class="visually-hidden">Heroes examples</h1>
 
-  <div class="px-4 py-2 my-4 text-center">
-    <img class="aspect-ratio-object-cover rounded mb-3 border" src="@if($tukang[0]->foto){{ url('storage/tukang/foto-profil/'.$tukang[0]->foto) }} @else https://t3.ftcdn.net/jpg/05/00/54/28/360_F_500542898_LpYSy4RGAi95aDim3TLtSgCNUxNlOlcM.jpg @endif" alt="" width="200">
-    <h2 class="display-6 fw-bold">{{ $tukang[0]->nama }}</h2>
-    <p class="mb-1 fs-5 fw-bolder">{{ $tukang[0]->nama_keahlian }}</p>
-    <p class="mb-3 mt-0 fs-6" style="font-size: 10pt;">
-      <i class="bi bi-geo-alt-fill">
-      </i> 
-      {{ $tukang[0]->kecamatan }}, {{ $tukang[0]->desa }}
-    </p>
-    <div class="col-lg-8 card border-left-primary shadow h-100 py-5 mx-auto">
-      <div class="text-start px-5">
-        <h5>Deskripsi</h5>
-        @if ($tukang[0]->deskripsi == null)
-        <p>Deskripsi tukang belum tersedia</p>
-        @else
-           <p class="mb-4">{!! $tukang[0]->deskripsi !!}</p>
-           @endif
-          </div>
-          <div class="col-md-12 text-start mb-5 px-5">
-      <h5>Pengalaman</h5>
-      @if (count($pengalaman) === 0)
-        <p>Belum mempunyai pengalaman</p>
-      @else
-                          <div class="py-1">
-                              <div class="card-body">
-                                  <div class="row text-start no-gutters align-items-center">
-                                      <div class="col-auto">
-                                          <img src="{{ url('storage/tukang/pengalaman/'.$pengalaman[0]->foto)}}" class="rounded" width="120px" alt="">
-                                      </div>
-                                      <div class="col mx-1">
-                                          <div class="h6 mb-0 fw-bold text-gray-800">{{ $pengalaman[0]->nama_proyek }}</div>
-                                          <div class="text-xs font-weight-bold text-gray-500 mb-1">
-                                             {{ $pengalaman[0]->alamat }}
-                                          </div>
-                                           <div class="text-xs fw-semibold text-gray mb-1" style="font-size: 10.5pt;">
-                                             <i class="bi bi-calendar-date" style="margin-right: 2px;"></i> {{ $pengalaman[0]->tanggal_mulai }} Sampai {{ $pengalaman[0]->tanggal_selesai }}  <i class="bi bi-briefcase-fill" style="margin: 0px 2px 0px 10px;"></i> {{ $pengalaman[0]->nama_keahlian }}
-                                          </div>
-                                           <div class="text-gray mb-1" style="font-size: 10pt;">
-                                             {{ $pengalaman[0]->deskripsi }}
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                          @endif
+    <div class="px-4 py-2 my-4 text-center">
+        <img class="aspect-ratio-object-cover rounded mb-3 border"
+            src="@if ($tukang->foto) {{ url('storage/tukang/foto-profil/' . $tukang->foto) }} @else https://t3.ftcdn.net/jpg/05/00/54/28/360_F_500542898_LpYSy4RGAi95aDim3TLtSgCNUxNlOlcM.jpg @endif"
+            alt="" width="200">
+        <h2 class="display-6 fw-bold">{{ $tukang->nama }}</h2>
+        <p class="mb-1 fs-5 fw-bolder">{{ $tukang->nama_keahlian }}</p>
+        <p class="mb-3 mt-0 fs-6" style="font-size: 10pt;">
+            <i class="bi bi-geo-alt-fill">
+            </i>
+            {{ ucwords(strtolower($kecamatan->name)) }}, {{ ucwords(strtolower($desa->name)) }}
+        </p>
+        <div class="col-lg-8 card border-left-primary shadow h-100 py-5 mx-auto">
+            <div class="text-start px-5">
+                <h5>Deskripsi</h5>
+                @if ($tukang->deskripsi == null)
+                    <p>Deskripsi tukang belum tersedia</p>
+                @else
+                    <p class="mb-4">{!! $tukang->deskripsi !!}</p>
+                @endif
+            </div>
+            <div class="col-md-12 text-start mb-5 px-5">
+                <h5>Pengalaman</h5>
+                @if (count($pengalaman) === 0)
+                    <p>Belum mempunyai pengalaman</p>
+                @else
+                    <div class="py-1">
+                        <div class="card-body">
+                            <div class="row text-start no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <img src="{{ url('storage/tukang/pengalaman/' . $pengalaman->foto) }}" class="rounded"
+                                        width="120px" alt="">
+                                </div>
+                                <div class="col mx-1">
+                                    <div class="h6 mb-0 fw-bold text-gray-800">{{ $pengalaman->nama_proyek }}</div>
+                                    <div class="text-xs font-weight-bold text-gray-500 mb-1">
+                                        {{ $pengalaman->alamat }}
+                                    </div>
+                                    <div class="text-xs fw-semibold text-gray mb-1" style="font-size: 10.5pt;">
+                                        <i class="bi bi-calendar-date" style="margin-right: 2px;"></i>
+                                        {{ $pengalaman->tanggal_mulai }} Sampai {{ $pengalaman->tanggal_selesai }} <i
+                                            class="bi bi-briefcase-fill" style="margin: 0px 2px 0px 10px;"></i>
+                                        {{ $pengalaman->nama_keahlian }}
+                                    </div>
+                                    <div class="text-gray mb-1" style="font-size: 10pt;">
+                                        {{ $pengalaman->deskripsi }}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <a type="button" class="btn btn-primary col-4 btn-lg mx-auto"  @if(Auth::check()) data-bs-toggle="modal" data-bs-target="#sewa" @else href="{{ route('auth.login') }}" @endif>Ajukan Sewa</a>
-      </div>
+                    </div>
+                @endif
+            </div>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <a type="button" class="btn btn-primary col-4 btn-lg mx-auto"
+                    @if (Auth::check()) data-bs-toggle="modal" data-bs-target="#sewa" @else href="{{ route('auth.login') }}" @endif>Ajukan
+                    Sewa</a>
+            </div>
 
-      
-      <div class="modal fade" id="sewa" tabindex="-1" aria-labelledby="sewaLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="sewaLabel">Form pengajuan sewa</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <p>Wajib di isi semua</p>
-      <form action="{{ route('pelanggan.sewa') }}" method="POST" class="modal-body text-start">
-        @csrf
-        <input type="hidden" name="tukangs_id" value="{{ $tukang[0]->id }}">
-        <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Tanggal Sewa</label>
-         <div id="datepicker" class="input-group date" data-date-format="mm-dd-yyyy">
-                                              <input class="form-control" name="tanggal_sewa" autofocus required type="text" />
-                                              <span class="input-group-addon input-group-text">
-                                                  <i class="bi bi-calendar-date"></i>
-                                              </span>
-                                          </div>
-      </div>
-      <div class="mb-3">
-      <select class="form-select" name="tipe_sewa" aria-label="Default select example">
-      <option selected>Pilih Tipe Sewa ...</option>
-      <option value="Harian">Harian</option>
-      <option value="Borongan Tenaga">Borongan Tenaga</option>
-      <option value="Borongan Penuh">Borongan Penuh</option>
-      </select>
-      </div>
-      <div class="mb-3">
-        <select class="form-select" name="tipe_bangunan" aria-label="Default select example">
-        <option selected>Pilih Tipe Bangunan ...</option>
-        <option value="Rumah">Rumah</option>
-        <option value="Kos">Kos</option>
-        <option value="Ruko/Kios">Ruko/Kios</option>
-        <option value="Restorant/Cafe">Restorant/Cafe</option>
-        <option value="Lainnya">Lainnya</option>
-        </select>
-      </div>
-      <div class="mb-3">
-      <select class="form-select" name="tipe_pengerjaan" aria-label="Default select example">
-      <option selected>Pilih Tipe Pengerjaan ...</option>
-      <option value="Pembangunan">Pembangunan</option>
-      <option value="Perbaikan">Perbaikan</option>
-      <option value="Renovasi">Renovasi</option>
-      <option value="Lainnya">Lainnya</option>
-      </select>
-      </div>
-      <div class="mb-3">
-        <select class="form-select" name="tipe_pembayaran" aria-label="Default select example">
-        <option selected>Pilih tipe pembayaran ...</option>
-        <option value="Bayar di tempat">Bayar di tempat</option>
-        <option value="Bank">Bank</option>
-      </select>
-      </div>
-    <textarea class="form-control" rows="3" name="deskripsi" placeholder="Deskripsikan kebutuahn sewa terhadap tukang.."></textarea>
-     
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-      <button type="submit" class="btn btn-primary">Ajukan sewa sekarang</button>
-    </form>
-      </div>
+
+            <div class="modal fade" id="sewa" tabindex="-1" aria-labelledby="sewaLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="sewaLabel">Form pengajuan sewa</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <p>Wajib di isi semua</p>
+                        <form action="{{ route('pelanggan.sewa') }}" method="POST" class="modal-body text-start">
+                            @csrf
+                            <input type="hidden" name="tukangs_id" value="{{ $tukang->id }}">
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Tanggal Sewa</label>
+                                <div id="datepicker" class="input-group date" data-date-format="mm-dd-yyyy">
+                                    <input class="form-control" name="tanggal_sewa" autofocus required type="text" />
+                                    <span class="input-group-addon input-group-text">
+                                        <i class="bi bi-calendar-date"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <select class="form-select" name="tipe_sewa" aria-label="Default select example">
+                                    <option selected>Pilih Tipe Sewa ...</option>
+                                    <option value="Harian">Harian</option>
+                                    <option value="Borongan Tenaga">Borongan Tenaga</option>
+                                    <option value="Borongan Penuh">Borongan Penuh</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <select class="form-select" name="tipe_bangunan" aria-label="Default select example">
+                                    <option selected>Pilih Tipe Bangunan ...</option>
+                                    <option value="Rumah">Rumah</option>
+                                    <option value="Kos">Kos</option>
+                                    <option value="Ruko/Kios">Ruko/Kios</option>
+                                    <option value="Restorant/Cafe">Restorant/Cafe</option>
+                                    <option value="Lainnya">Lainnya</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <select class="form-select" name="tipe_pengerjaan" aria-label="Default select example">
+                                    <option selected>Pilih Tipe Pengerjaan ...</option>
+                                    <option value="Pembangunan">Pembangunan</option>
+                                    <option value="Perbaikan">Perbaikan</option>
+                                    <option value="Renovasi">Renovasi</option>
+                                    <option value="Lainnya">Lainnya</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <select class="form-select" name="tipe_pembayaran" aria-label="Default select example">
+                                    <option selected>Pilih tipe pembayaran ...</option>
+                                    <option value="Bayar di tempat">Bayar di tempat</option>
+                                    <option value="Bank">Bank</option>
+                                </select>
+                            </div>
+                            <textarea class="form-control" rows="3" name="deskripsi"
+                                placeholder="Deskripsikan kebutuahn sewa terhadap tukang.."></textarea>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                <button type="submit" class="btn btn-primary">Ajukan sewa sekarang</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-    </div>      
-  </div>
-  @endsection
-  @section('js')
-<script type="text/javascript">
- $(function () {
-            $("#datepicker").datepicker({ 
-                autoclose: true, 
+    </div>
+@endsection
+@section('js')
+    <script type="text/javascript">
+        $(function() {
+            $("#datepicker").datepicker({
+                autoclose: true,
                 todayHighlight: true,
                 title: "Tanggal Mulai",
                 // todayBtn : "linked",
             }).datepicker('update', new Date());
         });
-</script>
-<script src=
-"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js">
     </script>
-    @endsection
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+@endsection
